e0f04777c49593d87309f57b9567781e9672ddf1
Sabrina <sabrinabudiono@gmail.com>
2026-01-14 16:20:44 +0100

    added some enhancement to the game logic

--- game_logic.py
+++ game_logic.py
@@ -21,12 +21,13 @@ def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
-def is_done(guessed_letters, secret_word):
-    for letter in guessed_letters:
-        if letter in secret_word and len(secret_word) == len(guessed_letters):
-            return True
-        else:
-            return False
+
+def game_won(guessed_letters, secret_word):
+    if len(secret_word) == len(guessed_letters):
+        return True
+    else:
+        return False
+
 
 def play_game():
     secret_word = get_random_word()
@@ -42,16 +43,22 @@ def play_game():
         if len(guess) == 1 and not guess.isdigit():
             if guess in secret_word:
                 guessed_letters.append(guess)
-                my_set = set(guessed_letters)
-                display_game_state(mistakes, secret_word, my_set)
-                if is_done(my_set, secret_word):
+                correct_guessed_letters = set(guessed_letters)
+                display_game_state(mistakes, secret_word, correct_guessed_letters)
+                if game_won(correct_guessed_letters, secret_word):
                     print("Congratulations, you saved the snowman!")
                     break
             elif guess not in guessed_letters:
                 mistakes += 1
                 if mistakes >= 3:
                     print(f"Game Over! The word was:{secret_word}")
-                    break
+                    print(ascii_art.STAGES[mistakes])
+                    user_input = input("Do you want to play again?(Y/N): ")
+                    if user_input == "N":
+                        break
+                    elif user_input == "Y":
+                        play_game()
+                        break
                 else:
                     display_game_state(mistakes, secret_word, guessed_letters)
         else:
