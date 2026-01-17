f9d7d443727f2e88cf9e5a7c7198e22c9c06fe79
Sabrina <sabrinabudiono@gmail.com>
2026-01-17 14:47:44 +0100

    latest update

--- game_logic.py
+++ game_logic.py
@@ -4,14 +4,14 @@ import ascii_art
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
 
-def display_game_state(mistakes, secret_word, guessed_letters):
+def display_game_state(mistakes, secret_word, correct_guessed_letters):
     """
     Display the snowman stage for the current number of mistakes.
     """
     print(ascii_art.STAGES[mistakes])
     display_word = ""
     for letter in secret_word:
-        if letter in guessed_letters:
+        if letter in correct_guessed_letters:
             display_word += letter + " "
         else:
             display_word += "_ "
@@ -26,11 +26,11 @@ def get_random_word():
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
-def game_won(guessed_letters, secret_word):
+def game_won(correct_guessed_letters, secret_word):
     """
-    returns True if the length of the guessed letter matches the secret word.
+    returns True if the length of the correctly guessed letter set matches length of the secret word set.
     """
-    if len(secret_word) == len(guessed_letters):
+    if len(set(secret_word)) == len(set(correct_guessed_letters)):
         return True
     else:
         return False
@@ -41,37 +41,31 @@ def play_game():
     starts the game loop.
     """
     secret_word = get_random_word()
-    guessed_letters = []
+    correct_guessed_letters = []
     mistakes = 0
     max_mistakes = 3
 
     print("Welcome to Snowman Meltdown!")
 
-    display_game_state(mistakes, secret_word, guessed_letters)
+    display_game_state(mistakes, secret_word, correct_guessed_letters)
 
     while True:
         guess = input("Guess a letter: ").lower()
-        if len(guess) == 1 and not guess.isdigit():
+        if len(guess) == 1 and not guess.isdigit() and guess.isalpha():
             if guess in secret_word:
-                guessed_letters.append(guess)
-                correct_guessed_letters = set(guessed_letters)
+                correct_guessed_letters.append(guess)
                 display_game_state(mistakes, secret_word, correct_guessed_letters)
                 if game_won(correct_guessed_letters, secret_word):
                     print("Congratulations, you saved the snowman!")
                     break
-            elif guess not in guessed_letters:
+            elif guess not in secret_word:
                 mistakes += 1
-                if mistakes >= max_mistakes:
+                if mistakes == max_mistakes:
                     print(f"Game Over! The word was:{secret_word}")
                     print(ascii_art.STAGES[mistakes])
-                    user_input = input("Do you want to play again?(Y/N): ")
-                    if user_input == "N":
-                        break
-                    elif user_input == "Y":
-                        play_game()
-                        break
+                    break
                 else:
-                    display_game_state(mistakes, secret_word, guessed_letters)
+                    display_game_state(mistakes, secret_word, correct_guessed_letters)
         else:
             print("Please enter a valid input. (one letter, no numbers, not empty")
             continue
--- snowman.py
+++ snowman.py
@@ -1,4 +1,11 @@
 import game_logic
 
-if __name__ == "__main__":
+def main():
     game_logic.play_game()
+    user_input = input("Do you want to play again?(Y/N): ")
+    if user_input == "Y":
+        game_logic.play_game()
+
+
+if __name__ == "__main__":
+    main()
