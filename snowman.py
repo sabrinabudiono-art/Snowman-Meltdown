7b8c19630d39e4a6305ebdbe10a591b9eab2ce31
Sabrina <sabrinabudiono@gmail.com>
2026-01-19 12:27:47 +0100

    update main function with play again function

--- snowman.py
+++ snowman.py
@@ -1,10 +1,22 @@
 import game_logic
 
+def play_again():
+    while True:
+        user_input = input("Do you want to play again?(Y/N): ")
+        if user_input.lower() == "y":
+            return True
+        elif user_input.lower() == "n":
+            return False
+        else:
+            print("Please enter a valid answer!")
+            continue
+
 def main():
-    game_logic.play_game()
-    user_input = input("Do you want to play again?(Y/N): ")
-    if user_input == "Y":
+    while True:
         game_logic.play_game()
+        if not play_again():
+            break
+
 
 
 if __name__ == "__main__":
